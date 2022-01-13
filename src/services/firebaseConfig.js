d4afd3d89755d1b4f3c77f9dd3c4ffae3a2300d8
JustinG <jtwgus@gmail.com>
2022-01-12 21:25:30 -0500

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAWfYB2aX2JVmlJkD8iFm0SLGFFNuxtKio',
-  authDomain: 'trailtracker-1060b.firebaseapp.com',
-  databaseURL: 'https://trailtracker-1060b.firebaseio.com',
-  projectId: 'trailtracker-1060b',
-  storageBucket: 'trailtracker-1060b.appspot.com',
-  messagingSenderId: '711544585577',
-  appId: '1:711544585577:web:3d49750d272a3a75b6d1f8',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
